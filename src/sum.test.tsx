5999f2ff1d5c69010226b1acc040285b6599b1b0
Sarah Portaria <sarah.portaria@gmail.com>
2022-07-06 16:12:06 -0400

    minimum setup for algo unit tests

---
+++ src/sum.test.tsx
@@ -0,0 +1,13 @@
+import { sum } from "./sum";
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+test('adds 2 + 2 to equal 4', () => {
+  expect(sum(2, 2)).toBe(4);
+});
+
+test('adds 2 + 1 to equal 3', () => {
+  expect(sum(2, 1)).toBe(3);
+});
